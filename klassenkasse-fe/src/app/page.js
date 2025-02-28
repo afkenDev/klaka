aceedfdc4265a2a470431c9d8c5e8ccc0f03f3d5
vincdeprinz <vincentstucki9@gmail.com>
2025-02-28 11:14:41 +0100

    navbar in bearbeitung

--- klassenkasse-fe/src/app/page.js
+++ klassenkasse-fe/src/app/page.js
@@ -1,9 +1,18 @@
-
+import Navbar from './components/Navbar'
 
 export default function Home() {
   return (
     <div>
-      hallo
+      <header>
+        <Navbar />
+      </header>
+      <main>
+        hallo
+      </main>
+      <footer>
+
+      </footer>
+
     </div>
   );
 }
